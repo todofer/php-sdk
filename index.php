00324535d505e684444a8a5f2c1211967b668c38
Maximiliano <57025020+todofer@users.noreply.github.com>
2021-09-06 11:18:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $appName = explode('.', $domain)[0];
 
         <div class="header">
             <div>
-                <h1>Getting Started with Mercado Libre's PHP SDK</h1>
+                <h1>Getting1 Started with Mercado Libre's PHP SDK</h1>
                 <h2>Official PHP SDK for Mercado Libre's API.</h2>
             </div>
         </div>
