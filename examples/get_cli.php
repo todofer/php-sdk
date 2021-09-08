b81524e4ff9c9188919c18744278f7dc6267ebe2
Maximiliano <57025020+todofer@users.noreply.github.com>
2021-09-08 12:02:33 -0300

    Update get_cli.php

--- examples/get_cli.php
+++ examples/get_cli.php
@@ -4,6 +4,14 @@ require '../configApp.php';
 
 $meli = new Meli($appId, $secretKey);
 
+
+
+$resource = 'resource_example'; // string | 
+$access_token = 'access_token_example'; // string | 
+
+curl -X GET -H 'Authorization: Bearer TG-6138c4c6bb17c300084fe110-380281361' 'https://api.mercadolibre.com/orders/4849969117/billing_info'
+
+
 $params = array();
 
 $url = 'https://api.mercadolibre.com/orders/4849135930/billing_info'; 
